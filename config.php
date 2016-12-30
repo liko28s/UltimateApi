14dc4cc9abbf4bd35331373987be2a73c1cf0db5
Ramiro Alvarez <liko28s@gmail.com>
2016-12-30 10:22:41 -0500

    - Configuracion inicial de Slim y DB (Eloquent)

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+const CONFIG = array(
+    "settings" => array(
+        "displayErrorDetails" => true,
+        "determineRouteBeforeAppMiddleware" => true,
+        'db' => [
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'database' => 'ultimate',
+            'username' => 'root',
+            'password' => 'root',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ]
+    )
+);
