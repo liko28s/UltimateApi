957e1ce27b5de3500be03d355632bd37a5a75dd1
Ramiro Alvarez <liko28s@gmail.com>
2016-12-30 15:58:18 -0500

    - Abstraccion del Super Modelo

---
+++ src/models/SuperModel.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Nextria\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SuperModel extends Model {
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+}
