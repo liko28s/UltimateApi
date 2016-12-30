30b2f7c6bfb647cf7baaed3dabc9a9dc1fec9348
Ramiro Alvarez <liko28s@gmail.com>
2016-12-30 12:10:57 -0500

    - Configuracion de la BD correcta - Dummy for testing

--- index.php
+++ index.php
@@ -4,7 +4,9 @@ include "config.php";
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Illuminate\Database\Capsule\Manager as CapsuleManager;
 use Nextria\Helpers\Logger as Logger;
+
 
 /** Instancia de Slim */
 $app = new \Slim\App(CONFIG);
@@ -22,6 +24,15 @@ $container['password'] = function () {
     return $_SERVER['PHP_AUTH_PW'];
 };
 
+/** Database */
+$container['db'] = function ($c) {
+  return $c['settings']['db'];
+};
+$capsule = new CapsuleManager();
+$capsule->addConnection($container->db);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
 //TODO añadir controlador de errores al contenedor
 
 /** Logger */
@@ -29,5 +40,13 @@ $container['logger'] = function ($c) {
     $logger = new Logger($c['userName']);
     return $logger->getInstance();
 };
+
+//Dummy Table TODO remove
+$app->get('/dummy', function (Request $request, Response $response) {
+    $dummy = new \Nextria\Controllers\DummyController();
+    return $response->withJson($dummy->getTwo());
+});
+
+//TODO instalador de la api para crear tablas etc
 
 $app->run();
