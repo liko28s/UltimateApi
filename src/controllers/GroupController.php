39fd321913c73c7d93173a69b3c0edbab4d791df
Ramiro Alvarez <liko28s@gmail.com>
2017-01-08 01:08:14 -0500

    PJ y PG de carreras (⊙_◎)

--- src/controllers/GroupController.php
+++ src/controllers/GroupController.php
@@ -3,6 +3,8 @@
 namespace Nextria\Controllers;
 
 use Nextria\Models\Group;
+use Nextria\Models\Match;
+use Nextria\Models\MatchDetails;
 
 class GroupController extends SuperController{
     public function __construct()
@@ -17,6 +19,32 @@ class GroupController extends SuperController{
             $groups = $this->model->get();
             foreach ($groups as $group) {
                 $group->teams = $this->model->find($group->id)->teams;
+                foreach ($group->teams as $team){
+                    $team->pj = Match::where('team_one',$team->id)
+                        ->where('match_time','<','current_timestamp')
+                        ->orWhere(function ($query){
+                            global $team;
+                            $query->where('team_two',$team->id)->where('match_time','<','current_timestamp');
+                        })
+                        ->count();
+                    $team->pg = 0;
+                    $details = MatchDetails::where('team_id',$team->id)->get();
+                    $matches = array();
+                    foreach ($details as $detail) {
+                        if($detail->event == 2) {
+                            if($detail->team_id == $team->id){
+                                $matches[$detail->match_id]['self']+=1;
+                            } else {
+                                $matches[$detail->match_id]['opponent']+=1;
+                            }
+                        }
+                    }
+                    foreach ($matches as $match){
+                        if($match['self']>$match['opponent']) {
+                            $team->pg++;
+                        }
+                    }
+                }
             }
             return $groups;
         }
